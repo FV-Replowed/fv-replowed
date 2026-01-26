183632b729421f95e8d710bcac476a3f1ae1795a
PuccamiteTech <53921171+PuccamiteTech@users.noreply.github.com>
2026-01-26 18:00:22 -0500

    Fix database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\UserMeta;
+use App\Models\UserAvatar;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +18,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $testUid = 1111111111;
 
-        User::factory()->create([
+        User::create([
+            'uid' => $testUid,
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => Hash::make('TestPassword'),
+        ]);
+
+        UserMeta::create([
+            'uid' => $testUid,
+            'firstName' => 'Test',
+            'lastName' => 'User',
+            
+            // TODO: make these schema defaults
+            'xp' => 0,
+            'cash' => 15,
+            'gold' => 500,
+            'energyMax' => 100,
+            'energy' => 100,
+            'seenFlags' => 'a:1:{s:13:"ftue_complete";b:0;}',
+            'isNew' => true,
+            "firstDay" => true
+        ]);
+
+        $userAvatar = UserAvatar::create([
+            'uid' => $testUid,
+            'value' => null
         ]);
     }
 }
