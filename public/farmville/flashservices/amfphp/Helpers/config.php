b13eb3d33ec741b6c51365d84565df02ff8571cb
jpfleischer <70083705+jpfleischer@users.noreply.github.com>
2026-02-15 03:39:20 -0500

    stick to layout of .env

--- public/farmville/flashservices/amfphp/Helpers/config.php
+++ public/farmville/flashservices/amfphp/Helpers/config.php
@@ -1,24 +1,12 @@
 <?php 
 
-// database credentials (prefer env when available)
-$dbHost = getenv('AMFPHP_DB_HOST');
-if ($dbHost === false || $dbHost === '') {
-    $dbHost = getenv('DB_HOST');
-}
-$dbUser = getenv('AMFPHP_DB_USERNAME');
-if ($dbUser === false || $dbUser === '') {
-    $dbUser = getenv('DB_USERNAME');
-}
-$dbPass = getenv('AMFPHP_DB_PASSWORD');
-if ($dbPass === false || $dbPass === '') {
-    $dbPass = getenv('DB_PASSWORD');
-}
-$dbName = getenv('AMFPHP_DB_NAME');
-if ($dbName === false || $dbName === '') {
-    $dbName = getenv('DB_DATABASE');
-}
+// database credentials (match .env.example / Laravel DB_* vars)
+$dbHost = getenv('DB_HOST') ?: '127.0.0.1';
+$dbUser = getenv('DB_USERNAME') ?: 'root';
+$dbPass = getenv('DB_PASSWORD') ?: '';
+$dbName = getenv('DB_DATABASE') ?: 'laratests';
 
-define('DB_SERVER', $dbHost ?: 'localhost');
-define('DB_USERNAME', $dbUser ?: '');
-define('DB_PASSWORD', $dbPass ?: '');
-define('DB_NAME', $dbName ?: '');
+define('DB_SERVER', $dbHost);
+define('DB_USERNAME', $dbUser);
+define('DB_PASSWORD', $dbPass);
+define('DB_NAME', $dbName);
